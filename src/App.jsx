19564dc0b3136d1ab093797c634762e7bbcb0cec
HEECHANG96 <wjdgmlckd96@daum.net>
2023-08-18 11:25:18 +0900

    Refactor: App => 메인 페이지로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,8 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import WeatherBox from "./components/WeatherBox";
-import WeatherButton from "./components/WeatherButton";
-import ClipLoader from "react-spinners/ClipLoader";
-import { getWeatherByCurrentLocation, getWeatherByCity } from "./api/api";
 import { Route, Routes } from "react-router-dom";
-import Main from "./pages/mainPage";
+import MainPage from "./pages/MainPage";
+
 // 1. 앱이 실행되자마자 현재 위치기반의 날씨가 보인다.
 // 2. 날씨정보에는 도시, 섭씨 화씨 날씨상태
 
@@ -17,74 +13,9 @@ import Main from "./pages/mainPage";
 // 6. 데이터를 들고오는 동안 로딩 스피너가 돈다.
 
 function App() {
-  const [weather, setWeather] = useState(null);
-  const [city, setCity] = useState("");
-  const [loading, setLoading] = useState(false);
-  const [apiError, setAPIError] = useState("");
-
-  const cities = ["Seoul", "Daegu", "Busan"];
-
-  const handleCityChange = (selectedCity) => {
-    if (selectedCity === "current") {
-      setCity("");
-    } else {
-      setCity(selectedCity);
-    }
-  };
-
-  // 1. UI가 그려지고 나서 호출
-  // 2. 배열에 있는 state 값이 바뀔 때마다 호출
-  useEffect(() => {
-    if (city === "") {
-      navigator.geolocation.getCurrentPosition((position) => {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        getWeatherByCurrentLocation(lat, lon, setLoading)
-          .then((data) => {
-            setWeather(data);
-          })
-          .catch((err) => {
-            setAPIError(err.message);
-          });
-      });
-    } else {
-      getWeatherByCity(city, setLoading)
-        .then((data) => {
-          setWeather(data);
-        })
-        .catch((err) => {
-          setAPIError(err.message);
-        });
-    }
-  }, [city]);
-
   return (
-    // <div className="app">
-    //   {loading ? (
-    //     <div className="container">
-    //       <ClipLoader
-    //         color="#f88c6b"
-    //         loading={loading}
-    //         size={150}
-    //         aria-label="Loading Spinner"
-    //         data-testid="loader"
-    //       />
-    //     </div>
-    //   ) : !apiError ? (
-    //     <div className="container">
-    //       <WeatherBox weather={weather} />
-    //       <WeatherButton
-    //         cities={cities}
-    //         setCity={setCity}
-    //         handleCityChange={handleCityChange}
-    //       />
-    //     </div>
-    //   ) : (
-    //     apiError
-    //   )}
-    // </div>
     <Routes>
-      <Route path="/" element={<Main />} />
+      <Route path="/" element={<MainPage />} />
     </Routes>
   );
 }
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -1,7 +1,77 @@
-import React from "react";
+import WeatherBox from "../components/WeatherBox";
+import WeatherButton from "../components/WeatherButton";
+import ClipLoader from "react-spinners/ClipLoader";
+import { useEffect, useState } from "react";
+import { getWeatherByCurrentLocation, getWeatherByCity } from "../api/api";
 
-const mainPage = () => {
-  return <div>메인 페이지</div>;
+const MainPage = () => {
+  const [weather, setWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [apiError, setAPIError] = useState("");
+
+  const cities = ["Seoul", "Daegu", "Busan"];
+
+  const handleCityChange = (selectedCity) => {
+    if (selectedCity === "current") {
+      setCity("");
+    } else {
+      setCity(selectedCity);
+    }
+  };
+
+  // 1. UI가 그려지고 나서 호출
+  // 2. 배열에 있는 state 값이 바뀔 때마다 호출
+  useEffect(() => {
+    if (city === "") {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+        getWeatherByCurrentLocation(lat, lon, setLoading)
+          .then((data) => {
+            setWeather(data);
+          })
+          .catch((err) => {
+            setAPIError(err.message);
+          });
+      });
+    } else {
+      getWeatherByCity(city, setLoading)
+        .then((data) => {
+          setWeather(data);
+        })
+        .catch((err) => {
+          setAPIError(err.message);
+        });
+    }
+  }, [city]);
+
+  return (
+    <div className="app">
+      {loading ? (
+        <div className="container">
+          <ClipLoader
+            color="#f88c6b"
+            loading={loading}
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : !apiError ? (
+        <div className="container">
+          <WeatherBox weather={weather} />
+          <WeatherButton
+            cities={cities}
+            setCity={setCity}
+            handleCityChange={handleCityChange}
+          />
+        </div>
+      ) : (
+        apiError
+      )}
+    </div>
+  );
 };
 
-export default mainPage;
+export default MainPage;
