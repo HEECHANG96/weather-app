dd44b558ba1fff927cdcb244078be0e81ee13b53
HEECHANG96 <wjdgmlckd96@daum.net>
2023-09-03 22:23:31 +0900

    Fix: 유료 API부분 삭제

--- src/api/api.js
+++ src/api/api.js
@@ -2,7 +2,6 @@ import axios from "axios";
 
 const API_KEY = "1bd3b506be86774ab15096d112352a91";
 const API_BASE_URL = "https://api.openweathermap.org/data/2.5/weather";
-const API_BASE_URL2 = "https://api.openweathermap.org/data/2.5/forecast/daily";
 
 const fetchWeatherData = async (url) => {
   try {
@@ -26,17 +25,6 @@ export const getWeatherByCurrentLocation = async (lat, lon) => {
 
 export const getWeatherByCity = async (city) => {
   const url = `${API_BASE_URL}?q=${city}&appid=${API_KEY}&units=metric`;
-
-  try {
-    const data = await fetchWeatherData(url);
-    return data;
-  } catch (err) {
-    throw err;
-  }
-};
-
-export const getDailyForecastWeatherData = async (lat, lon) => {
-  const url = `${API_BASE_URL2}?lat=${lat}&lon=${lon}&cnt=7&appid=${API_KEY}`;
 
   try {
     const data = await fetchWeatherData(url);
--- src/pages/mainPage.jsx
+++ src/pages/mainPage.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import CurrentDate from "../components/Date/CurrentDate.jsx";
 import Search from "../components/Search/Search.jsx";
 import Location from "../components/Location/Location.jsx";
-import DetailInfo from "../components/DetailInfo/DetailInfo.jsx";
 
 const mainPage = () => {
   return (
     <div>
-      <DetailInfo />
       <Search />
       <CurrentDate />
       <Location />
